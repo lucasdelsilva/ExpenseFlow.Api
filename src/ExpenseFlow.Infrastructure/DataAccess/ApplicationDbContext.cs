b08a83126e343fd70de74d4930f5da09bbe929fd
Lucas Silva <silva.lucasdel@outlook.com>
2025-04-30 23:33:16 -0300

    Add new table (Tags) and migration

--- src/ExpenseFlow.Infrastructure/DataAccess/ApplicationDbContext.cs
+++ src/ExpenseFlow.Infrastructure/DataAccess/ApplicationDbContext.cs
@@ -8,4 +8,11 @@ public class ApplicationDbContext : DbContext
 
     public DbSet<Expense> Expenses { get; set; }
     public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Tag>().ToTable("Tags");
+    }
 }
